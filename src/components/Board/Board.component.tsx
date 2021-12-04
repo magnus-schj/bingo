e079a70c198b09748ae4af19af93b8f0128d72e7
Magnus <beachmagnus+git@gmail.com>
2021-12-04 16:04:04 +0100

    mobile responsiveness

--- src/components/Board/Board.component.tsx
+++ src/components/Board/Board.component.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@mui/material";
 import React, { FC } from "react";
 import { useAppSelector } from "../../App/hookts";
 import { auth } from "../../firebase/firebase.utils";
@@ -11,23 +12,26 @@ interface Props {}
 const Board: FC<Props> = () => {
   const currentUserSlice = useAppSelector((state) => state.currentUser);
 
+  const pad = useMediaQuery("(max-width:542px)");
   return (
-    <div>
-      <ul>
-        {currentUserSlice.board &&
-          currentUserSlice.board.map((row: Square[], i) => (
-            <div key={i} className="row">
-              {row.map(({ id, ...data }) => (
-                <SquareComponent
-                  key={id}
-                  uId={currentUserSlice.userInfo?.uid}
-                  sId={id}
-                  {...data}
-                />
-              ))}
-            </div>
-          ))}
-      </ul>
+    <div
+      style={{
+        width: pad ? "95%" : "auto",
+      }}
+    >
+      {currentUserSlice.board &&
+        currentUserSlice.board.map((row: Square[], i) => (
+          <div key={i} className="row">
+            {row.map(({ id, ...data }) => (
+              <SquareComponent
+                key={id}
+                uId={currentUserSlice.userInfo?.uid}
+                sId={id}
+                {...data}
+              />
+            ))}
+          </div>
+        ))}
     </div>
   );
 };
--- src/components/Square/Square.component.tsx
+++ src/components/Square/Square.component.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 
 import "./Square.styles.css";
-import { ButtonBase, Card } from "@mui/material";
+import { ButtonBase, Card, useMediaQuery, Typography } from "@mui/material";
 import { markAsHappened } from "../../firebase/firebase.utils";
 
 interface Props {
@@ -13,17 +13,24 @@ interface Props {
 
 const SquareComponent: FC<Props> = ({ event, uId, sId, happened }) => {
   if (!uId) return null;
-
+  const pad = useMediaQuery("(max-width:524px)");
+  const cardDimension = pad ? "4rem" : "8rem";
+  const dimension = pad ? "2rem" : "4rem";
   return (
-    <Card>
+    <Card
+      raised
+      sx={{ margin: "1rem", height: cardDimension, width: cardDimension }}
+    >
       <ButtonBase
         onClick={() => markAsHappened(uId, sId)}
         disabled={happened}
         sx={{
-          padding: "4rem",
           background: happened ? "gold" : "white",
+          height: "100%",
+          width: "100%",
         }}
       >
+        <Typography variant="subtitle2" color="initial"></Typography>
         {event}
       </ButtonBase>
     </Card>
